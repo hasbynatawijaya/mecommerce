01e27071a22195180fe52107bb684587f4272504
hasbynatawijaya <mhasbyn@gmail.com>
2025-03-11 22:40:10 +0700

    fix unescaped entities

--- app/unauthorized/page.tsx
+++ app/unauthorized/page.tsx
@@ -12,7 +12,7 @@ const UnauthorizedPage = () => {
     <div className="container mx-auto flex flex-col items-center justify-center space-y-4 h-[calc(100vh-200px)]">
       <h1 className="h1-bold text-4xl">Unauthorized access</h1>
       <p className="text-muted-foreground">
-        You don't have permission to access this page
+        {`You don't have permission to access this page`}
       </p>
       <Button asChild>
         <Link href="/">Return Home</Link>
